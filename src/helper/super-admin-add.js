a0f911f1e77432dcda51bb2d32361dadea76dfe1
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-27 00:40:48 +0500

    up

--- src/helper/super-admin-add.js
+++ src/helper/super-admin-add.js
@@ -1,14 +1,27 @@
 import config from '../config/server.config.js'
 import { connectDB } from '../databases/server.database.js'
+import { disconnect } from 'mongoose'
+import { Admin } from '../models/admin.schema.js'
+import Crypt from '../utils/Crypt.js'
+
 (async function () {
     try {
         await connectDB()
+        const isRole = await Admin.findOne({ role: 'SUPERADMIN' })
+        if (isRole) {
+            console.log('SUPERADMIN already added :(');
+            return
+        }
+        const hashPassword = Crypt.encrypt(config.SUPERADMIN.password)
         const admin = {
             username: config.SUPERADMIN.username,
             email: config.SUPERADMIN.email,
-            password: config.SUPERADMIN.password
+            hashPassword,
+            role: 'SUPERADMIN'
         }
-        // await /
+        await Admin.create(admin)
+        console.log('Admin is created :)');
+        await disconnect()
     } catch (error) {
         console.log('Error create SUPERADMIN:', error.message)
     }
