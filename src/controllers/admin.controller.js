34c571b8104b1329536d2f03bf390e8965e53c1b
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-27 00:24:28 +0500

    up

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -30,7 +30,7 @@ class AdminController extends BaseController {
             const hashPassword = await Crypt.encrypt(password)
             //req.body parili o'chirib tashlavoti
             delete req.body.password
-            //create qilivoti databasaga
+            //create qilinvoti databasaga
             const result = await Admin.create(...req.body, hashPassword)
             successRes(res, result, 201)
         } catch (error) {
@@ -41,20 +41,27 @@ class AdminController extends BaseController {
     signIn = async (req, res, next) => {
         try {
             const { email, password } = req.body;
+            //email bor yoki yoqligini aniqlavomiza!
             const admin = await Admin.findOne({ email })
             if (!admin) {
                 throw new AppError('Email or password incorrect', 401)
             }
+            //parol t'ogri ekanligini aniqlavomiza!
             const hashPassword = await Crypt.decrypt(password, admin.hashPassword);
             if (!hashPassword) {
                 throw new AppError('Email or password incorrect', 401)
             }
+            // token ga data bervomiza
             const payload = {
                 id: admin._id, isActive: admin.isActive, role: admin.role
             }
+            //access token ovomiza
             const access = Token.accessToken(payload);
+            //refresh token ovomiza
             const refresh = Token.refreshToken(payload);
+            // cookie ga refresh tokini yozib qoyvomiza
             Token.writeCookie(res, 'refreshToken', refresh, 30);
+            //return qilinvoti frontend ga
             return successRes(res, {
                 token: access,
                 data: admin
