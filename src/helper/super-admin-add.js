0cef02dcc6e6a24e632513d3bc7339630aa41268
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-27 00:43:59 +0500

    up

--- src/helper/super-admin-add.js
+++ src/helper/super-admin-add.js
@@ -9,10 +9,10 @@ import Crypt from '../utils/Crypt.js'
         await connectDB()
         const isRole = await Admin.findOne({ role: 'SUPERADMIN' })
         if (isRole) {
-            console.log('SUPERADMIN already added :(');
-            return
+            console.log('SUPERADMIN already added :(\nDatabase is disconnect');
+            process.exit(1)
         }
-        const hashPassword = Crypt.encrypt(config.SUPERADMIN.password)
+        const hashPassword = await Crypt.encrypt(config.SUPERADMIN.password)
         const admin = {
             username: config.SUPERADMIN.username,
             email: config.SUPERADMIN.email,
@@ -20,8 +20,9 @@ import Crypt from '../utils/Crypt.js'
             role: 'SUPERADMIN'
         }
         await Admin.create(admin)
-        console.log('Admin is created :)');
+        console.log('Admin is created :)\nDatabase is disconnect');
         await disconnect()
+        process.exit(1)
     } catch (error) {
         console.log('Error create SUPERADMIN:', error.message)
     }
