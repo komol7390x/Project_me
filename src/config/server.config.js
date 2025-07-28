c68872ae871057a749bc23b01e6d54606d7bf67a
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-28 09:17:07 +0500

    up

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -16,5 +16,11 @@ export default {
         password: process.env.SUPERADMIN_PASSWORD,
         username: process.env.SUPERADMIN_USERNAME,
         role: process.env.SUPERADMIN_ROLE
+    },
+    GMAIL: {
+        MAIL_HOST: process.env.MAIL_HOST,
+        MAIL_PORT: process.env.MAIL_PORT,
+        MAIL_USER: process.env.MAIL_USER,
+        MAIL_PASSWORD: process.env.MAIL_PASSWORD
     }
 }
