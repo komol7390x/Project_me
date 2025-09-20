f0d3a70ed2a9903fec2ed5ae8242d81d3b79e73f
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-09-20 14:44:25 +0500

    bot

--- src/error/not-found-page.js
+++
@@ -1,6 +0,0 @@
-import { AppError } from "./AppError.js";
-
-export const pageError = (req, _res, next) => {
-    const messageError = req.url.split('/')[1] == 'uploads' ? 'File not found' : 'Page not found'
-    throw next(new AppError(messageError, 404))
-}
